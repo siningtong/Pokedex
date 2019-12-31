c250202636178dee8ea6239613b3de12c39f0270
siningtong <siningtong@gmail.com>
2019-12-31 18:43:45 +0000

    add css style to pokecard and pokedex component

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import './Pokecard.css'
+import React, { Component } from "react";
+import "./Pokecard.css";
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`
-    const { id, name, type, base_experience } = this.props
+    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    const { id, name, type, base_experience } = this.props;
     return (
       <div className="Pokecard">
-        <h1>{name}</h1>
+        <h1 className="Pokecard-title">{name}</h1>
         <img src={imgSrc} alt={this.props.name} />
-        <div>Type:{type}</div>
-        <div>EXP:{base_experience}</div>
-      </div >
-
-    )
+        <div className="Pokecard-data">Type:{type}</div>
+        <div className="Pokecard-data">EXP:{base_experience}</div>
+      </div>
+    );
   }
 }
 export default Pokecard;
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,31 +1,34 @@
 import React, { Component } from "react";
 import Pokecard from "./Pokecard";
+import "./Pokedex.css";
 
 class Pokedex extends Component {
   static defaultProps = {
     pokemon: [
-      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
-      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
-      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
-      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+      { id: 8, name: "Charmander", type: "fire", base_experience: 62 },
+      { id: 66, name: "Squirtle", type: "water", base_experience: 63 },
+      { id: 88, name: "Metapod", type: "bug", base_experience: 72 },
+      { id: 16, name: "Butterfree", type: "flying", base_experience: 178 },
       { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
       { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
-      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
-      { id: 133, name: "Eevee", type: "normal", base_experience: 65 }
+      { id: 99, name: "Gengar", type: "poison", base_experience: 225 },
+      { id: 100, name: "Eevee", type: "normal", base_experience: 65 }
     ]
   };
 
   render() {
     return (
       <div className="Pokedex">
-        {this.props.pokemon.map(data => (
-          <Pokecard
-            id={data.id}
-            name={data.name}
-            type={data.type}
-            base_experience={data.base_experience}
-          />
-        ))}
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map(data => (
+            <Pokecard
+              id={data.id}
+              name={data.name}
+              type={data.type}
+              base_experience={data.base_experience}
+            />
+          ))}
+        </div>
       </div>
     );
   }
